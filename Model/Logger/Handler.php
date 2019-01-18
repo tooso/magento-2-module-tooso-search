f791ae8681893a7fbcce73548ad987c9ab4fdc6b
Alessandro Ronchi <aleron75@gmail.com>
2019-01-18 17:15:13 +0100

    Add strict type mode directive and fix a wrong parameter

--- Model/Logger/Handler.php
+++ Model/Logger/Handler.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 namespace Bitbull\Tooso\Model\Logger;
 
 use Magento\Framework\Logger\Handler\Base;
--- Plugin/Model/Layer/Search/CollectionFilter/ApplyToosoSearch.php
+++ Plugin/Model/Layer/Search/CollectionFilter/ApplyToosoSearch.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Bitbull\Tooso\Plugin\Model\Layer\Search\CollectionFilter;
 
