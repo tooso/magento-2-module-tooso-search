90811daea140fc485119f6b93ea07bf9b9c8e643
Fabio Gollinucci <fbgolly@gmail.com>
2019-01-23 16:15:48 +0100

    Add non cacheable block in search result page to prevent cache

---
+++ Block/Tracking/ClickAfterSearch.php
@@ -0,0 +1,10 @@
+<?php
+namespace Bitbull\Tooso\Block\Tracking;
+
+class ClickAfterSearch extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+}
