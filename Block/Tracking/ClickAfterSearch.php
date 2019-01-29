4a5a98c346f492bf23f3a58463ab0d152124c4d8
Fabio Gollinucci <fbgolly@gmail.com>
2019-01-29 17:28:20 +0100

    Fix debug output when search is disabled

--- Block/Tracking/ClickAfterSearch.php
+++ Block/Tracking/ClickAfterSearch.php
@@ -1,27 +1,36 @@
 <?php
 namespace Bitbull\Tooso\Block\Tracking;
 
+use Bitbull\Tooso\Api\Service\ConfigInterface;
 use Bitbull\Tooso\Api\Service\SearchInterface;
 use Magento\Framework\View\Element\Template\Context;
 
 class ClickAfterSearch extends \Magento\Framework\View\Element\Template
 {
     /**
-     * @var SearchInterface|null
+     * @var SearchInterface
      */
-    public $search = null;
+    public $search;
+
+    /**
+     * @var ConfigInterface
+     */
+    public $config;
 
     /**
      * ClickAfterSearch constructor.
      *
      * @param Context $context
      * @param SearchInterface $search
+     * @param ConfigInterface $config
      */
     public function __construct(
         Context $context,
-        SearchInterface $search
+        SearchInterface $search,
+        ConfigInterface $config
     ) {
         parent::__construct($context);
         $this->search = $search;
+        $this->config = $config;
     }
 }
