a2a9ca4f76dc3d0dd7fb8e4b58d4c649e9991eb1
Fabio Gollinucci <fbgolly@gmail.com>
2019-02-28 17:11:40 +0100

    Add reindex sync procedures

--- Model/Indexer/Stock.php
+++ Model/Indexer/Stock.php
@@ -1,11 +1,8 @@
 <?php declare(strict_types=1);
 namespace Bitbull\Tooso\Model\Indexer;
 
+use Bitbull\Tooso\Api\Service\Indexer\StockInterface;
 use Bitbull\Tooso\Api\Service\LoggerInterface;
-use Bitbull\Tooso\Model\Indexer\Operation\OperationFactory;
-use Bitbull\Tooso\Model\Indexer\Decorator\CatalogDecoratorFactory;
-use Bitbull\Tooso\Model\Indexer\Decorator\CategoriesDecoratorFactory;
-use Bitbull\Tooso\Model\Indexer\Decorator\VariantsDecoratorFactory;
 
 class Stock implements \Magento\Framework\Indexer\ActionInterface, \Magento\Framework\Mview\ActionInterface
 {
@@ -17,13 +14,21 @@ class Stock implements \Magento\Framework\Indexer\ActionInterface, \Magento\Fram
     protected $logger;
 
     /**
+     * @var StockInterface
+     */
+    protected $stock;
+
+    /**
      * @param LoggerInterface $logger
+     * @param StockInterface $stock
      */
     public function __construct(
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        StockInterface $stock
     )
     {
         $this->logger = $logger;
+        $this->stock = $stock;
     }
 
     /*
@@ -32,6 +37,8 @@ class Stock implements \Magento\Framework\Indexer\ActionInterface, \Magento\Fram
     public function execute($ids){
         //Used by mview, allows you to process multiple placed orders in the "Update on schedule" mode
         $this->logger->debug('[indexer stock] asked to execute reindex for: '.implode(',', $ids));
+        $this->stock->execute($ids);
+        $this->logger->debug('[indexer stock] done!');
     }
 
     /*
@@ -41,6 +48,8 @@ class Stock implements \Magento\Framework\Indexer\ActionInterface, \Magento\Fram
     public function executeFull(){
         //Should take into account all placed orders in the system
         $this->logger->debug('[indexer stock] asked to execute a full reindex');
+        $this->stock->execute();
+        $this->logger->debug('[indexer stock] done!');
     }
 
 
@@ -50,6 +59,8 @@ class Stock implements \Magento\Framework\Indexer\ActionInterface, \Magento\Fram
     public function executeList(array $ids){
         //Works with a set of placed orders (mass actions and so on)
         $this->logger->debug('[indexer stock] asked to execute reindex on specific entity: '.implode(',', $ids));
+        $this->stock->execute($ids);
+        $this->logger->debug('[indexer stock] done!');
     }
 
 
@@ -59,5 +70,7 @@ class Stock implements \Magento\Framework\Indexer\ActionInterface, \Magento\Fram
     public function executeRow($id){
         //Works in runtime for a single order using plugins
         $this->logger->debug('[indexer stock] asked to execute a reindex for single entity: '.$id);
+        $this->stock->execute([$id]);
+        $this->logger->debug('[indexer stock] done!');
     }
 }
