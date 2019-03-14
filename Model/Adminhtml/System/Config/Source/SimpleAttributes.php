7eb97986d51a7a0bd1ef24126d0257586438c67c
Fabio Gollinucci <fbgolly@gmail.com>
2019-03-14 11:51:23 +0100

    Add default attributes, remove default attributes from configurations

--- Model/Adminhtml/System/Config/Source/SimpleAttributes.php
+++ Model/Adminhtml/System/Config/Source/SimpleAttributes.php
@@ -9,6 +9,13 @@ use Magento\Framework\Api\Search\FilterGroupBuilder;
 class SimpleAttributes implements \Magento\Framework\Option\ArrayInterface
 {
     /**
+     * @var array
+     */
+    const DEFAULT_ATTRIBUTES = [
+        'sku', 'name', 'description', 'price', 'short_description'
+    ];
+
+    /**
      * @var AttributeRepository
      */
     protected $attributeRepository;
@@ -54,43 +61,63 @@ class SimpleAttributes implements \Magento\Framework\Option\ArrayInterface
     {
         $searchCriteria = $this->searchCriteriaBuilder->create();
         $searchCriteria->setFilterGroups([
-             $this->filterGroupBuilder
+            $this->filterGroupBuilder
                 ->addFilter(
                     $this->filterBuilder->setField('backend_type')
-                      ->setValue([
+                        ->setValue([
                             'varchar',
                             'int',
                             'text',
                             'decimal',
                         ])
-                      ->setConditionType('in')
-                      ->create()
+                        ->setConditionType('in')
+                        ->create()
                 )
+                ->create(),
+            $this->filterGroupBuilder
                 ->addFilter($this->filterBuilder->setField('frontend_input')
-                      ->setValue([
-                            'text',
-                            'textarea',
-                            'multiselect',
-                            'select',
-                            'boolean',
-                            'price'
-                        ])
-                      ->setConditionType('in')
-                      ->create()
+                    ->setValue([
+                        'text',
+                        'textarea',
+                        'multiselect',
+                        'select',
+                        'boolean',
+                        'price'
+                    ])
+                    ->setConditionType('in')
+                    ->create()
                 )
-                ->create()
+                ->create(),
+            $this->filterGroupBuilder
+                ->addFilter($this->filterBuilder->setField('attribute_code')
+                    ->setValue($this->getDefaultAttributes())
+                    ->setConditionType('nin')
+                    ->create())
+                ->create(),
         ]);
 
         $attributeRepository = $this->attributeRepository->getList($searchCriteria);
         $attributes = [];
 
         foreach ($attributeRepository->getItems() as $attribute) {
+            $frontendLabel = $attribute->getFrontendLabel();
+            $attributeCode = $attribute->getAttributeCode();
             $attributes[] = [
-                'label' => $attribute->getFrontendLabel(),
-                'value' => $attribute->getAttributeCode(),
+                'label' => "$frontendLabel ($attributeCode)",
+                'value' => $attributeCode,
             ];
         }
 
         return $attributes;
+    }
+
+    /**
+     * Get default attributes
+     *
+     * @return array
+     */
+    public function getDefaultAttributes()
+    {
+        return self::DEFAULT_ATTRIBUTES;
     }
 }
