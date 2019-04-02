9b333fa92aa2a1ad6ec6bbc9377ff4ac1dc231b7
Fabio Gollinucci <fbgolly@gmail.com>
2019-04-02 10:23:20 +0200

    Add on click event handler with different approach

--- view/frontend/web/js/clickAfterSearch.js
+++ view/frontend/web/js/clickAfterSearch.js
@@ -12,85 +12,79 @@ define([
     var config = {};
 
     /**
-     * Product links elements
-     *
-     * @type {Array}
-     */
-    var elements = [];
-
-    /**
-     * Find alla products links into container
-     */
-    function elaborateProductsElements(parentContainer) {
-        elements = [];
-        $(parentContainer).find(config.productLinksSelector).each(function () {
-            var element = $(this);
-            elements.push(element);
-            var productSku = element.attr(config.attributeName);
-            var linkValue = element.attr('href');
-            element.attr('data-href', linkValue);
-            element.attr('href', '#');
-            attachClickHandler(element, productSku, linkValue);
-        });
-    }
-
-    /**
-     * Attach click handler to element
+     * Get element position
      *
      * @param {*} element
-     * @param {String} productSku
-     * @param {String} linkValue
+     * @param {*} parent
+     * @return {Number}
      */
-    function attachClickHandler(element, productSku, linkValue) {
-        element.click(function () {
-            if (window.ta === undefined) {
-                console.warn('Tooso: ta is not include but analytics is active');
-                document.location.href = linkValue; // this should never happens
-                return;
-            }
-
-            var product = config.products[productSku];
-            if (product === undefined) {
-                product = {
-                    id: productSku,
-                    position: elements.indexOf(element) + (config.pageSize * (config.currentPage - 1))
-                }
-            }
-            if (config.debug) {
-                console.debug('Tooso: click after search captured');
-            }
-            ta('ec:addProduct', product);
-            if (config.debug) {
-                console.debug('Tooso: tracked product:', product);
-            }
-            ta('ec:setAction', 'click', {
-                'list': config.searchId
-            });
-            var timeout = setTimeout(function () {
-                console.warn('Tooso: tracking system does not respond in time');
-                document.location.href = linkValue; // fallback in case the library did not respond in time
-            }, 1000);
-            ta('send', 'event', 'cart', 'click', {
-                hitCallback: function () {
-                    clearTimeout(timeout);
-                    if (config.debug) {
-                        console.debug('Tooso: redirecting to ' + linkValue);
-                    }
-                    document.location.href = linkValue;
-                },
-            });
-        })
+    function getElementPosition(element, parent) {
+        var links = $(parent).find(config.productLinksSelector);
+        return links.toArray().indexOf(element);
     }
 
     /**
-     * Observer for HTML changes
+     * Click handler for product link elements
      *
-     * @param {*} target
-     * @param {function} callback
+     * @param {*} event
      */
-    function observeForChanges(target, callback) {
-        var observer = new MutationObserver(callback);
-        observer.observe(target, { attributes: true, childList: true, characterData: true, subtree: true });
+    function clickHandler(event) {
+        var element = $(this);
+        var linkValue = element.attr('href');
+        var productSku = element.attr(config.attributeName);
+
+        if (config.debug) {
+            console.debug('Tooso: click after search captured');
+        }
+
+        if (window.ta === undefined) {
+            console.warn('Tooso: ta is not include but analytics is active');
+            return;
+        }
+        if (linkValue === null || linkValue === undefined) {
+            console.warn('Tooso: click handled on a non-link element, href attribute not found');
+            return;
+        }
+        if (productSku === null || productSku === undefined) {
+            console.warn('Tooso: product link does not have the attribute '.config.attributeName);
+            return;
+        }
+
+        elaboratePaginationFromURL();
+
+        var product = config.products[productSku];
+        if (product === undefined) {
+            product = {
+                id: productSku,
+                position: getElementPosition(element.get(0), event.data.parent) + (config.pageSize * (config.currentPage - 1))
+            }
+        }
+        ta('ec:addProduct', product);
+        if (config.debug) {
+            console.debug('Tooso: tracked product:', product);
+        }
+        ta('ec:setAction', 'click', {
+            'list': config.searchId
+        });
+
+        var timeout = setTimeout(function () {
+            console.warn('Tooso: tracking system does not respond in time');
+            document.location.href = linkValue; // fallback in case the library did not respond in time
+        }, 1000);
+        ta('send', 'event', 'cart', 'click', {
+            hitCallback: function () {
+                clearTimeout(timeout);
+                if (config.debug) {
+                    console.debug('Tooso: redirecting to ' + linkValue);
+                }
+                document.location.href = linkValue;
+            },
+        });
+
+        // Prevent default click behaviour
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
     }
 
     /**
@@ -120,29 +114,21 @@ define([
     /**
      * Configuration function
      *
-     * @param {Object} config
-     * @param {*} element
+     * @param {Object} configParams
+     * @param {*} parentContainer
      */
     return function (configParams, parentContainer) {
         config = Object.assign({}, configParams);
         config.pageSize = config.pageSize || 0;
         config.currentPage = config.currentPage || 1;
 
-        // Elaborate child elements
-        elaborateProductsElements(parentContainer);
-
-        // Observer for HTML changes (like AJAX pagination,filters..)
-        observeForChanges(parentContainer, function () {
-            if (config.debug) {
-                console.debug('Tooso: products container changed');
-            }
-
-            // Check for pagination change
-            elaboratePaginationFromURL();
-
-            // Re-elaborate child elements
-            elaborateProductsElements(parentContainer);
-        })
-
+        $(parentContainer).on(
+            'click',
+            config.productLinksSelector,
+            {
+                parent: parentContainer
+            },
+            clickHandler
+        );
     }
 });
