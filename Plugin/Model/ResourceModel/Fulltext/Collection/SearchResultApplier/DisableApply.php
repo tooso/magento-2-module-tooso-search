1a651733cbb0124478fa17fd074ce756f02980c5
Enrico Naletto <en4letto@gmail.com>
2020-04-24 18:42:20 +0200

    Fix layered navigation not working on category pages

--- Plugin/Model/ResourceModel/Fulltext/Collection/SearchResultApplier/DisableApply.php
+++ Plugin/Model/ResourceModel/Fulltext/Collection/SearchResultApplier/DisableApply.php
@@ -4,6 +4,7 @@ namespace Bitbull\Tooso\Plugin\Model\ResourceModel\Fulltext\Collection\SearchRes
 
 use Bitbull\Tooso\Api\Service\ConfigInterface;
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection\SearchResultApplier;
+use Magento\Framework\App\RequestInterface;
 
 class DisableApply
 {
@@ -11,20 +12,37 @@ class DisableApply
      * @var ConfigInterface
      */
     protected $config;
+    /**
+     * @var RequestInterface
+     */
+    private $request;
 
     /**
      * @param ConfigInterface $config
+     * @param RequestInterface $request
      */
     public function __construct(
-        ConfigInterface $config
+        ConfigInterface $config,
+        RequestInterface $request
     ) {
         $this->config = $config;
+        $this->request = $request;
     }
 
     public function aroundApply(SearchResultApplier $subject, \Closure $next)
     {
-        if ($this->config->isSearchEnabled() !== true) {
+        if ($this->isToosoSearchDisabled() || $this->isNotSearchPage()) {
             $next();
         }
+    }
+
+    private function isToosoSearchDisabled()
+    {
+        return $this->config->isSearchEnabled() !== true;
+    }
+
+    private function isNotSearchPage()
+    {
+        return $this->request->getFullActionName() !== 'catalogsearch_result_index';
     }
 }
